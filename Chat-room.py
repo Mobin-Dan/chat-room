a8aa8370b3ea18a6e8e22aeb0d37e93a1b161c21
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2020-07-20 00:57:52 +0430

    CHaT-room
    You can build a personal chat room

---
+++ Chat-room.py
@@ -0,0 +1,8 @@
+print('////////////////////////')
+print('coding by mobin.dan')
+print('youtub{https://youtu.be/XkTfV-31c3c}')
+print('\\\\\\\\\\\\\\\\\\')
+import webbrowser
+x =input('entet your chat room name :')
+URL1 ='http://hack.chat/?'+x
+webbrowser.open(URL1)
